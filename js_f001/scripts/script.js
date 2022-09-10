fb481859d1d502bb39c09486d887a48509c25b02
Simão Pedro Mendes Gabriel <spedromg@hotmail.com>
2022-09-10 12:42:35 -0300

    update

---
+++ js_f001/scripts/script.js
@@ -0,0 +1,5 @@
+function somaValores() {
+  number1 = Number(document.getElementById('numb1').value);
+  number2 = Number(document.getElementById('numb2').value);
+  document.getElementById('result').value = number1 + number2;
+}
